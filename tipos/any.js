0878d5b01bea90e8e14e4c22d4d8922750204ec1
Mario Ochoa <mario120gdl@gmail.com>
2022-01-10 18:26:30 -0600

    Any hecho

---
+++ tipos/any.js
@@ -0,0 +1,13 @@
+"use strict";
+(() => {
+    let avenger = 123;
+    let exist;
+    let power;
+    avenger = 'Dr Strange';
+    // console.log(avenger.charAt(0))
+    console.log(avenger.charAt(0));
+    avenger = 150.23256415;
+    console.log(avenger.toFixed(2));
+    console.log(exist);
+    console.log(power);
+})();
